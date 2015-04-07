ca79a7dfae1e7b88982895e79d317a664aa5ec02
Carlos Cerrillos <carlos@cerrillostech.com>
2015-04-07 08:00:28 -0700

    not needed

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 
 error_reporting(E_ALL);
-ini_set('display_errors', '1');
+//ini_set('display_errors', '1');
 
 include_once("func/config.func.php");
 $siteEnabled = siteEnabled();
