376bbc6ef4df4f8dace89de25af8265863f36dc8
Carlos Cerrillos <carlos@cerrillostech.com>
2016-03-21 22:18:43 -0700

    all locations

--- public/api/1.0/locations_api.php
+++ public/api/1.0/locations_api.php
@@ -12,7 +12,8 @@ class Location {
   }
 }
 
-$app->get('/locations/{location_id}', function (Request $request, Response $response) {
+function GetLocation($location_id) {
+  $data = null;
   global $db_host, $db_user, $db_pass, $db_name;
   $mysqli = new mysqli($db_host, $db_user, $db_pass);
   $mysqli -> select_db($db_name);
@@ -20,20 +21,46 @@ $app->get('/locations/{location_id}', function (Request $request, Response $resp
     echo "Connection Failed: " . mysqli_connect_errno();
     exit();
   }
-  $location_id = $request->getAttribute('location_id');
   if($stmt = $mysqli -> prepare("SELECT `location_name` FROM `locations` WHERE `location_id` = ? LIMIT 1;")) {
     $stmt->bind_param("i", intval($location_id));
     $stmt->execute();
     $stmt->bind_result($location_name);
     while($stmt->fetch()) {
-      $response->getBody()->write(json_encode(new Location($location_id, $location_name), JSON_PRETTY_PRINT));
+      $data = new Location($location_id, $location_name);
     }
-
+    $stmt->close();
   } else {
-    $response->getBody()->write($mysqli->error);
+    echo $mysqli->error;
   }
-    
-    
+  return $data;
+}
+
+$app->get('/locations/', function (Request $request, Response $response) {
+  $data = array();
+  global $db_host, $db_user, $db_pass, $db_name;
+  $mysqli = new mysqli($db_host, $db_user, $db_pass);
+  $mysqli -> select_db($db_name);
+  if(mysqli_connect_errno()) {
+    echo "Connection Failed: " . mysqli_connect_errno();
+    exit();
+  }
+  if($stmt = $mysqli -> prepare("SELECT `location_id`, `location_name` FROM `locations`;")) {
+    $stmt->execute();
+    $stmt->bind_result($location_id, $location_name);
+    while($stmt->fetch()) {
+      $data[$location_id] = new Location($location_id, $location_name);
+    }
+    $stmt->close();
+  } else {
+    echo $mysqli->error;
+  }
+  $response->getBody()->write(json_encode($data, JSON_PRETTY_PRINT));
+  return $response;
+});
+
+$app->get('/locations/{location_id}', function (Request $request, Response $response) {
+  $data = GetLocation($request->getAttribute('location_id'));
+  $response->getBody()->write(json_encode($data, JSON_PRETTY_PRINT));
 
   return $response;
 });
