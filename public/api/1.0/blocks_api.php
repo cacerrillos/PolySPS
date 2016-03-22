1100428336de1291458fc699143f6e627a976792
Carlos Cerrillos <carlos@cerrillostech.com>
2016-03-21 22:22:37 -0700

    all blocks api

--- public/api/1.0/blocks_api.php
+++ public/api/1.0/blocks_api.php
@@ -12,6 +12,29 @@ class Block {
   }
 }
 
+$app->get('/blocks/', function (Request $request, Response $response) {
+  $data = array();
+  global $db_host, $db_user, $db_pass, $db_name;
+  $mysqli = new mysqli($db_host, $db_user, $db_pass);
+  $mysqli -> select_db($db_name);
+  if(mysqli_connect_errno()) {
+    echo "Connection Failed: " . mysqli_connect_errno();
+    exit();
+  }
+  if($stmt = $mysqli -> prepare("SELECT `block_id`, `block_name` FROM `blocks`;")) {
+    $stmt->execute();
+    $stmt->bind_result($block_id, $block_name);
+    while($stmt->fetch()) {
+      $data[$block_id] = new Block($block_id, $block_name);
+    }
+  } else {
+    $response->getBody()->write($mysqli->error);
+  }
+
+  $response->getBody()->write(json_encode($data, JSON_PRETTY_PRINT));
+  return $response;
+});
+
 $app->get('/blocks/{block_id}', function (Request $request, Response $response) {
   global $db_host, $db_user, $db_pass, $db_name;
   $mysqli = new mysqli($db_host, $db_user, $db_pass);
