78399c9bde6231d70ce047bde919ffe5d0cd1364
Carlos Cerrillos <carlos@cerrillostech.com>
2016-03-28 14:50:17 -0700

    Grade levels api

--- public/api/1.0/grade_levels_api.php
+++ public/api/1.0/grade_levels_api.php
@@ -64,4 +64,71 @@ $app->get('/grade_levels/{grade_id}', function (Request $request, Response $resp
   return $response;
 });
 
+$app->post('/grade_levels/', function(Request $request, Response $response) {
+  $status = array();
+  $status['status'] = false;
+  $post_data = $request->getParsedBody();
+  if(isset($post_data['grade_name'])) {
+    //var_dump($all_gl);
+    $mysqli = $this->db;
+
+    if($stmt = $mysqli->prepare("INSERT INTO `grade_levels` (`grade_id`, `grade_name`) VALUES (NULL, ?);")) {
+      $stmt->bind_param("s", $post_data['grade_name']);
+      $stmt->execute();
+      if($stmt->affected_rows == 1) {
+        $status['status'] = true;
+      }
+      $stmt->close();
+    } else {
+      echo $mysqli->error;
+    }
+  }
+  $response->getBody()->write(json_encode($status, JSON_PRETTY_PRINT));
+  return $response;
+});
+
+$app->put('/grade_levels/', function(Request $request, Response $response) {
+  $status = array();
+  $status['status'] = false;
+  $post_data = $request->getParsedBody();
+  //try to save data
+  if(isset($post_data['grade_id']) && isset($post_data['grade_name'])) {
+    $mysqli = $this->db;
+
+    if($stmt = $mysqli->prepare("UPDATE `grade_levels` SET `grade_name` = ? WHERE `grade_levels`.`grade_id` = ? LIMIT 1;")) {
+      $stmt->bind_param("si", $post_data['grade_name'], $post_data['grade_id']);
+      $stmt->execute();
+      if($stmt->affected_rows == 1) {
+        $status['status'] = true;
+      }
+      $stmt->close();
+    } else {
+      echo $mysqli->error;
+    }
+  }
+  $response->getBody()->write(json_encode($status, JSON_PRETTY_PRINT));
+  return $response;
+});
+
+$app->delete('/grade_levels/', function (Request $request, Response $response) {
+  $resp = array();
+  $resp['status'] = false;
+  $post_data = $request->getParsedBody();
+    $mysqli = $this->db;
+  foreach ($post_data as $key => $value) {
+    if($stmt = $mysqli->prepare("DELETE FROM `grade_levels` WHERE `grade_id` = ?;")) {
+      $stmt->bind_param("i", $value['grade_id']);
+      $stmt->execute();
+      if($stmt->affected_rows > 0) {
+        $resp['status'] = true;
+      }
+      $stmt->close();
+    } else {
+      echo $mysqli->error;
+    }
+  }
+  $response->getBody()->write(json_encode($resp, JSON_PRETTY_PRINT));
+  return $response;
+});
+
 ?>
