9194ac888565dacef598035a045847d6c45c6029
Carlos Cerrillos <carlos@cerrillostech.com>
2016-03-29 19:34:31 -0700

    dev-prod

--- config.php
+++ config.php
@@ -1,6 +1,17 @@
 <?php
-$db_host = "localhost:23306";
-$db_user = "sps";
-$db_pass = "sps";
-$db_name = "sps";
+$prod = false;
+if($prod) {
+  $db_host = "***REMOVED***";
+  $db_user = "***REMOVED***";
+  $db_pass = "***REMOVED***";
+  $db_name = "***REMOVED***";
+} else {
+  $db_host = "localhost:23306";
+  $db_user = "sps";
+  $db_pass = "sps";
+  $db_name = "sps";
+}
+
+
+
 ?>
