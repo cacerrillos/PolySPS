5c22586296b46fda705aa61a793c84286e20844a
Carlos Cerrillos <carlos@cerrillostech.com>
2016-05-04 04:05:06 -0700

    one enabled status

--- public/api/1.0/index.php
+++ public/api/1.0/index.php
@@ -59,11 +59,7 @@ $set_tings['enabled12'] = GetSetting($container['db'], "enabled12");
 
 function can_register($grade_id) {
   global $set_tings;
-  if($grade_id == 1) {
-    return $set_tings['enabled11']->entry_value == "1";
-  } else if($grade_id == 2) {
-    return $set_tings['enabled12']->entry_value == "1";
-  }
+  return $set_tings['enabled11']->entry_value == "1";
   return false;
 }
 
